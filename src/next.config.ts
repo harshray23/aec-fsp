4e6043b6f36c81a61f2b169c2c9986aac02b88c2
Harsh Ray <harshray2007@gmail.com>
2025-06-12 11:51:21 +0000

    studio-6317169159:~/studio{main}$ npm run dev
    > nextn@0.1.0 dev
    > next

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      'https://9003-firebase-studio-1748625231387.cluster-fkltigo73ncaixtmokrzxhwsfc.cloudworkstations.dev',
+    ],
+  },
+};
+
+export default nextConfig;
