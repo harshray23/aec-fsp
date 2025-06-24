ca6dc382e313530b6733f9026f0d465c639c60a8
Harsh Ray <harshray2007@gmail.com>
2025-06-24 10:26:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/admin/BatchEditForm.tsx
+++ src/components/admin/BatchEditForm.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
