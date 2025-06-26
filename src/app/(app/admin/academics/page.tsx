e9ba6ecaaa48ddc52a7d9383f2debd49ea95f926
Harsh Ray <harshray2007@gmail.com>
2025-06-26 13:09:16 +0000

    i deleted that file now resolve the error

--- src/app/(app/admin/academics/page.tsx
+++
@@ -1,6 +0,0 @@
-//
-// THIS FILE HAS AN INVALID PATH AND IS CRASHING THE SERVER.
-//
-// Please DELETE this file and its parent folder, which has an invalid name.
-// The correct, working file is located at: /src/app/(app)/admin/academics/page.tsx
-//
