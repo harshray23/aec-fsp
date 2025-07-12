1f77a08117d472f822e922e424244a9983d9562d
Harsh Ray <harshray2007@gmail.com>
2025-07-12 07:18:40 +0000

    i added all the images in public folder

--- src/next.config.ts
+++ src/next.config.ts
@@ -9,16 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
   allowedDevOrigins: [
     'https://9003-firebase-studio-1748625231387.cluster-fkltigo73ncaixtmokrzxhwsfc.cloudworkstations.dev',
   ],
